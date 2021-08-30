5d19414d809c00fc56f86dd898c654e36199e4d4
Your Name <your.email@example.com>
2021-08-31 00:11:00 +0900

    add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from werkzeug import exceptions
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -9,6 +10,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+import wikipedia
 
 app = Flask(__name__)
 
@@ -18,6 +20,7 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+wikipedia.set_lang("ja")
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,9 +43,26 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    send_message = event.message.text
+    try:
+        wikipedia_page = wikipedia.page(send_message)
+        wikipedia_title=wikipedia_page.title
+        wikipedia_url=wikipedia_page.url
+        wikipedia_summary=wikipedia.summary(send_message)
+        reply_message = '【' + wikipedia_title + '】\n' + wikipedia_summary + '\n\n' + '【詳しくはこちら】\n' + wikipedia_url
+
+    except wikipedia.exceptions.PageError:
+        reply_message ='【' + send_message + '】\nについての情報は見つかりませんでした。'
+
+    except wikipedia.exceptions.DisambiguationError as e:
+        disambiguation_list = e.options
+        reply_message = '複数の候補が返ってきました。以下の候補から、お探しの用語に近いものを再入力してください。\n\n'
+        for word in disambiguation_list:
+            reply_message += '・' + word + '\n'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(reply_message)
+    )
 
 
 if __name__ == "__main__":
